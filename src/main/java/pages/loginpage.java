2bda8e9a233c8640e2ccfdd0c173b7b87fc4ebdb
phanidevi <phanidevi0@>
2023-08-23 18:16:51 +0530

    first commit

---
+++ src/main/java/pages/loginpage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class loginpage {
+	
+	
+public void Loginpage() {
+	
+	System.out.println("this is login page");
+}
+}
---
+++ src/test/java/testcases/logintest.java
@@ -0,0 +1,7 @@
+package testcases;
+
+public class logintest {
+public void LoginpageTest() {
+	System.out.println("this is loginpage test");
+}
+}
